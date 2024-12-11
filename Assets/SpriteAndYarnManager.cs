88c5125d01018d1bd598a0df1e7d2ae412d0855a
Gryph0123 <gryphninja@gmail.com>
2024-12-10 22:45:37 -0500

    Added finishing touches

--- Assets/SpriteAndYarnManager.cs
+++ Assets/SpriteAndYarnManager.cs
@@ -9,22 +9,29 @@ public class SpriteAndYarnManager : MonoBehaviour
     public DialogueRunner runner;
     public GameObject MrDocken;
     public GameObject Nova;
+    int counter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        runner.StartDialogue("MrDocken");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!runner.IsDialogueRunning)
+        if ((!runner.IsDialogueRunning)&&counter==0)
+        {
+            runner.StartDialogue("MrDocken");
+            MrDocken.SetActive(true);
+            counter++;
+        }
+        else if ((!runner.IsDialogueRunning)&& counter!=0)
         {
             runner.StartDialogue("Nova");
             Nova.SetActive(true);
             MrDocken.SetActive(false);
         }
+
     }
 }
