417523355b817465e020a8786795b227e0326ab8
Gryph0123 <gryphninja@gmail.com>
2024-12-10 21:14:59 -0500

    Added in sprites and stuff

--- Assets/SpriteAndYarnManager.cs
+++ Assets/SpriteAndYarnManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class SpriteAndYarnManager : MonoBehaviour
 {
+
+    public DialogueRunner runner;
+    public GameObject MrDocken;
+    public GameObject Nova;
+
     // Start is called before the first frame update
     void Start()
     {
+        runner.StartDialogue("MrDocken");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!runner.IsDialogueRunning)
+        {
+            runner.StartDialogue("Nova");
+            Nova.SetActive(true);
+            MrDocken.SetActive(false);
+        }
     }
 }
